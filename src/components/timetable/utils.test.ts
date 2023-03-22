725a724c5730e145163b8d1db93fd9915e0a1567
Simon Hurley <simon.hurley@formidable.com>
2023-03-22 23:28:52 +0000

    Added `getPrettyDateRange` unit tests Added `getTimetableTitle` unit tests

--- src/components/timetable/utils.test.ts
+++ src/components/timetable/utils.test.ts
@@ -1,4 +1,9 @@
-import { getFirstDayOfWeek, getLongMonthName } from "./utils";
+import {
+  getFirstDayOfWeek,
+  getLongMonthName,
+  getPrettyDateRange,
+  getTimetableTitle,
+} from "./utils";
 
 describe("getLongMonthName", () => {
   test.each([
@@ -29,5 +34,47 @@ describe("getFirstDayOfWeek", () => {
     ],
   ])("%s: getFirstDayOfWeek(%s) => %s", (testTitle, date, expected) => {
     expect(getFirstDayOfWeek(date)).toEqual(expected);
+  });
+});
+
+describe("getPrettyDateRange", () => {
+  test.each([
+    [
+      "Only from date",
+      new Date(2023, 2, 19, 0, 0, 0, 0),
+      undefined,
+      "From Sunday, March 19, 2023",
+    ],
+    [
+      "Only to date",
+      undefined,
+      new Date(2022, 11, 26, 0, 0, 0, 0),
+      " to Monday, December 26, 2022",
+    ],
+    [
+      "From and to dates",
+      new Date(2022, 11, 14, 0, 0, 0, 0),
+      new Date(2022, 11, 12, 0, 0, 0, 0),
+      "From Wednesday, December 14, 2022 to Monday, December 12, 2022",
+    ],
+  ])("%s: getFirstDayOfWeek(%s,%s) => %s", (testTitle, from, to, expected) => {
+    expect(getPrettyDateRange(from, to)).toEqual(expected);
+  });
+});
+
+describe("getTimetableTitle", () => {
+  test.each([
+    ["No tags", [], " Activities"],
+    ["1 tag", [{ name: "tag 1", type: "type 1" }], "Tag 1 Activities"],
+    [
+      "2 tags",
+      [
+        { name: "tag 1", type: "type 1" },
+        { name: "tag 2", type: "type 2" },
+      ],
+      "Tag 1, Tag 2 Activities",
+    ],
+  ])("%s: getTimetableTitle(%s) => %s", (testTitle, tags, expected) => {
+    expect(getTimetableTitle(tags)).toEqual(expected);
   });
 });
